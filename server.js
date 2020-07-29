b99a133c21ab325f1e422a342b373f07fe513616
Purevdorj Batgerel <purvee.hq@gmail.com>
2020-07-29 16:09:00 -0500

    chore: replace express with live-server

--- server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const app = express()
-
-app.use(express.static('public'));
-
-app.get('/', (req, res) => res.sendFile('index.html'));
-
-app.listen(3000, ()=>console.log('server started on port 3000'));
