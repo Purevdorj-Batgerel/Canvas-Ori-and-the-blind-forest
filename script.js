4a388532f5f924617e15af4c9ff5542af199326a
Purevdorj Batgerel <purvee.hq@gmail.com>
2021-12-14 19:26:59 -0600

    chore: remove svg files

--- script.js
+++ script.js
@@ -21,18 +21,8 @@ const audio = new Audio("./assets/ori_main_theme.mp3");
 
 const logo = new Image();
 const pattern = new Image();
-const clickIcon = new Image();
-const volumeUpIcon = new Image();
-const volumeOffIcon = new Image();
-const fullScreenIcon = new Image();
-const fullScreenExitIcon = new Image();
 logo.src = "./assets/logo1.png";
 pattern.src = "./assets/raster.png";
-clickIcon.src = "./assets/click.svg";
-volumeUpIcon.src = "./assets/volumeUp.svg";
-volumeOffIcon.src = "./assets/volumeOff.svg";
-fullScreenIcon.src = "./assets/fullscreen.svg";
-fullScreenExitIcon.src = "./assets/fullscreenExit.svg";
 
 const backCtx = backCanvas.getContext("2d", { alpha: false });
 const foreCtx = foreCanvas.getContext("2d");
@@ -54,6 +44,9 @@ let fadeProgress = 1;
 let isSoundInitialized = false;
 
 let minRatio = 1;
+
+const FULLSCREEN_ICON_SIZE = 60;
+const VOLUME_ICON_SIZE = 48;
 
 const PARTICLE_COUNT = Math.floor(
   (window.innerHeight * window.innerWidth) / 40000
@@ -320,12 +313,6 @@ const drawUI = (time) => {
   uiCtx.fillRect(0, 0, radius * 2, radius * 2);
   uiCtx.restore();
 
-  const clickRatio = minRatio + 1;
-  const clickWidth = clickIcon.naturalWidth / clickRatio;
-  const clickHeight = clickIcon.naturalHeight / clickRatio;
-  const clickWOffset = (window.innerWidth - clickWidth) / 2 - 5;
-  const clickHOffset = window.innerHeight * 0.86;
-
   if (time) {
     let timeFraction = (time - startTime) / FADE_DURATION;
     if (timeFraction < 0) {
@@ -340,55 +327,73 @@ const drawUI = (time) => {
 
   uiCtx.save();
   uiCtx.globalAlpha = fadeProgress;
+  uiCtx.fillStyle = "white";
 
-  uiCtx.drawImage(
-    clickIcon,
-    Math.ceil(clickWOffset),
-    Math.ceil(clickHOffset),
-    Math.ceil(clickWidth),
-    Math.ceil(clickHeight)
+  const TEMP_SCALE = 5 + minRatio;
+  uiCtx.scale(1 / TEMP_SCALE, 1 / TEMP_SCALE);
+  uiCtx.translate(
+    ((window.innerWidth - 75) * TEMP_SCALE) / 2,
+    window.innerHeight * 0.85 * TEMP_SCALE
   );
+
+  uiCtx.fill(
+    new Path2D(
+      "M319 188h-1c-9 0-18 3-24 9-6-15-20-26-36-26-10 0-20 4-27 11-6-13-19-21-34-21-8-1-16 2-22 7V76c0-23-18-41-39-41S98 53 98 76v182l-10-12c-10-11-23-18-37-19-15-1-28 3-39 13l-9 8c-3 2-4 6-2 9l88 169c16 30 46 50 79 50h99v-1c50 0 90-43 90-96V228c0-23-17-40-38-40zm22 135v56c0 44-34 81-75 81h-98c-27 0-52-16-65-42L18 256l5-4a36 36 0 0152 4l25 30a8 8 0 0014-5V76c0-14 10-25 22-25s23 11 23 25v178a8 8 0 0016 0v-54c0-13 10-23 22-23 13 0 23 10 23 24v48a8 8 0 0016 0v-38c0-13 9-24 22-24 12 0 22 11 22 24v36a8 8 0 0016 0v-19c0-14 10-24 22-24h1c12 0 22 10 22 24v95zm0 0"
+    )
+  );
+  uiCtx.fill(
+    new Path2D(
+      "M73 79c4 0 8-3 8-8a55 55 0 01110 0 8 8 0 0016 0 71 71 0 00-142 0c0 5 3 8 8 8zm0 0"
+    )
+  );
+
   uiCtx.restore();
 
   uiCtx.save();
   uiCtx.globalAlpha = 1 - fadeProgress;
   drawTexts();
+  uiCtx.save();
+  uiCtx.fillStyle = "white";
+  uiCtx.scale(2, 2);
+  uiCtx.translate(24 / 2, 24 / 2);
   if (audio.muted) {
-    uiCtx.drawImage(
-      volumeOffIcon,
-      24,
-      24,
-      volumeOffIcon.width,
-      volumeOffIcon.height
+    uiCtx.fill(
+      new Path2D(
+        "M16.5 12c0-1.77-1.02-3.29-2.5-4.03v2.21l2.45 2.45c.03-.2.05-.41.05-.63zm2.5 0c0 .94-.2 1.82-.54 2.64l1.51 1.51C20.63 14.91 21 13.5 21 12c0-4.28-2.99-7.86-7-8.77v2.06c2.89.86 5 3.54 5 6.71zM4.27 3L3 4.27 7.73 9H3v6h4l5 5v-6.73l4.25 4.25c-.67.52-1.42.93-2.25 1.18v2.06c1.38-.31 2.63-.95 3.69-1.81L19.73 21 21 19.73l-9-9L4.27 3zM12 4L9.91 6.09 12 8.18V4z"
+      )
     );
   } else {
-    uiCtx.drawImage(
-      volumeUpIcon,
-      24,
-      24,
-      volumeUpIcon.width,
-      volumeUpIcon.height
+    uiCtx.fill(
+      new Path2D(
+        "M3 9v6h4l5 5V4L7 9H3zm13.5 3c0-1.77-1.02-3.29-2.5-4.03v8.05c1.48-.73 2.5-2.25 2.5-4.02zM14 3.23v2.06c2.89.86 5 3.54 5 6.71s-2.11 5.85-5 6.71v2.06c4.01-.91 7-4.49 7-8.77s-2.99-7.86-7-8.77z"
+      )
     );
   }
   uiCtx.restore();
+  uiCtx.restore();
+
+  uiCtx.save();
+  uiCtx.fillStyle = "white";
+  uiCtx.scale(2.5, 2.5);
+  uiCtx.translate(
+    (window.innerWidth - 16 - FULLSCREEN_ICON_SIZE) / 2.5,
+    16 / 2.5
+  );
 
   if (document.fullscreenElement) {
-    uiCtx.drawImage(
-      fullScreenExitIcon,
-      window.innerWidth - fullScreenIcon.width - 16,
-      16,
-      fullScreenIcon.width,
-      fullScreenIcon.height
+    uiCtx.fill(
+      new Path2D(
+        "M5 16h3v3h2v-5H5v2zm3-8H5v2h5V5H8v3zm6 11h2v-3h3v-2h-5v5zm2-11V5h-2v5h5V8h-3"
+      )
     );
   } else {
-    uiCtx.drawImage(
-      fullScreenIcon,
-      window.innerWidth - fullScreenIcon.width - 16,
-      16,
-      fullScreenIcon.width,
-      fullScreenIcon.height
+    uiCtx.fill(
+      new Path2D(
+        "M7 14H5v5h5v-2H7v-3zm-2-4h2V7h3V5H5v5zm12 7h-3v2h5v-5h-2v3zM14 5v2h3v3h2V5h-5z"
+      )
     );
   }
+  uiCtx.restore();
 
   uiCtx.save();
   const ctxPattern = uiCtx.createPattern(pattern, "repeat");
@@ -426,10 +431,10 @@ const clickHandler = (event) => {
   const { clientX, clientY } = event;
 
   if (
-    window.innerWidth - fullScreenIcon.width - 16 < clientX &&
+    window.innerWidth - FULLSCREEN_ICON_SIZE - 16 < clientX &&
     clientX < window.innerWidth - 16 &&
     clientY > 16 &&
-    clientY < 16 + fullScreenIcon.height
+    clientY < 16 + FULLSCREEN_ICON_SIZE
   ) {
     if (document.fullscreenElement) {
       document.exitFullscreen();
@@ -438,11 +443,10 @@ const clickHandler = (event) => {
     }
   } else if (
     16 < clientX &&
-    clientX < 16 + volumeUpIcon.width &&
+    clientX < 16 + VOLUME_ICON_SIZE &&
     16 < clientY &&
-    clientY < 16 + volumeUpIcon.height
+    clientY < 16 + VOLUME_ICON_SIZE
   ) {
-    console.log("VOLUME");
     audio.muted = !audio.muted;
     drawUI();
   } else {
