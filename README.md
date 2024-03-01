cb7fff9c568e5b0891e352010b9d7b1929bebaff
Purevdorj Batgerel <purvee.hq@gmail.com>
2024-02-29 21:02:49 -0600

    fix: use inline css for performance improvement

--- README.md
+++ README.md
@@ -9,14 +9,13 @@ Web canvas learning project
 ### Number of style files (css, sass, styl):
 Zero
 
+used minimal inline css for performance improvement
+
 ### Used third party libraries for animation, sound, particle, parallax effect:
 None
 
 ## Live preview
 https://purevdorj-batgerel.github.io/Canvas-Ori-and-the-blind-forest/
-
-## Font
-[Montserrat](https://fonts.google.com/specimen/Montserrat)
 
 ## Music
 Gareth Coker, Ori and the Blind Forest, Main Theme - Definitive Edition
--- main.js
+++ main.js
@@ -8,10 +8,6 @@ import WispCanvas from "./src/WispCanvas";
 
 import { fadeStartTime, setDimensionRatio } from "./src/globalValues";
 
-document.body.style.position = "fixed";
-document.body.style.margin = 0;
-document.body.style.font = "12px Verdana";
-
 const audioManager = new AudioManager();
 const inputManager = new InputManager(audioManager);
 
@@ -38,26 +34,26 @@ const draw = (time) => {
   window.requestAnimationFrame(draw);
 };
 
-// window.onload = () => {
-//   window.addEventListener("resize", () => {
-//     setDimensionRatio();
-//     canvasManager.resizeCanvases();
-//     canvasManager.draw();
-//   });
-//   window.addEventListener("deviceorientation", (event) => {
-//     inputManager.handleDeviceOrientation(event);
-//     parallaxCanvas.draw();
-//   });
-//   window.addEventListener("mousemove", (event) => {
-//     inputManager.handleMouseMove(event);
-//     parallaxCanvas.draw();
-//   });
+window.onload = () => {
+  window.addEventListener("resize", () => {
+    setDimensionRatio();
+    canvasManager.resizeCanvases();
+    canvasManager.draw();
+  });
+  window.addEventListener("deviceorientation", (event) => {
+    inputManager.handleDeviceOrientation(event);
+    parallaxCanvas.draw();
+  });
+  window.addEventListener("mousemove", (event) => {
+    inputManager.handleMouseMove(event);
+    parallaxCanvas.draw();
+  });
 
-//   const topMostCanvas = canvasManager.getCanvasElements().at(-1);
-//   topMostCanvas.addEventListener("click", inputManager.handleClick);
+  const topMostCanvas = canvasManager.getCanvasElements().at(-1);
+  topMostCanvas.addEventListener("click", inputManager.handleClick);
 
-//   canvasManager.appendToDom();
-//   canvasManager.draw();
+  canvasManager.appendToDom();
+  canvasManager.draw();
 
-//   draw();
-// };
+  draw();
+};
